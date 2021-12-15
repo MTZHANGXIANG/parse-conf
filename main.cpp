b8f74d854d580a604971782c53018a5c0028780d
MTZHANGXIANG <1184833224@qq.com>
2021-12-15 05:11:26 -0500

    版本1.0.0合入

--- main.cpp
+++ main.cpp
@@ -6,14 +6,23 @@
 #include "config.h" 
 
 
-#define ADCCONFIGFILES  "../iio-hwmon.conf"   // ADCconf文件所在的文件位置
-#define ADCMAXNUM  16                         // ADCconf文件中包含adc数量的最大值
 
+struct sensorInfo {
+    char *type;
+    char *sensorNameLabel;
+    char *sensorWLlabel;
+    char *sensorWHlabel;
+    char *sensorCLlabel;
+    char *sensorCHlabel;
+    char *sensorPath;
+    int sensorMaxNum;
+};
 
-enum sensor {
-    ADC = 1,
-    TEMPURATURE,
-    AMPER,
+struct sensorInfo tsensorInfo[] = {
+    {"ADC", "LABEL_in", "WARNLO_in", "WARNHI_in", "CRITLO_in", "CRITHI_in", "../iio-hwmon.conf", 1},
+    {"TEMPURATURE", "LABEL_temp", "WARNLO_temp", "WARNHI_temp", "CRITLO_temp", "CRITHI_temp", "../tmp421@1e.conf", 1},
+    {"TEMPURATURE", "LABEL_temp", "WARNLO_temp", "WARNHI_temp", "CRITLO_temp", "CRITHI_temp", "../tmp421@2a.conf", 1},
+    {"TEMPURATURE", "LABEL_temp", "WARNLO_temp", "WARNHI_temp", "CRITLO_temp", "CRITHI_temp", "../tmp421@4e.conf", 1}
 };
 
 void printHelp(void)
@@ -26,90 +35,112 @@ void printHelp(void)
     printf("                      if the params-1 is INTERVAL, the params is the INTERVAL value(millisecond)\n");
     printf("            params-3: sensor's threshold' value, the value is multiply  1000\n");
     printf("                      if the params-1 is INTERVAL, the params-3 is NULL\n");
+    printf("  -q        modify the curr, tempature sensor threshlod, the params is same as adc\n");
+    printf("  -t        modify the interval time\n");
     
 }
 
-uint8_t setSensorInterval(char *str, uint8_t sensortype)
+void addquota(char *str)
 {
-    char ConfigFile[100] = {'\0'};
+    char tmp[50] = {'\0'};
+    int i = 0;
 
-    if (sensortype == ADC)
+    tmp[i] = '"';
+    for (i = 0; str[i] != '\0'; i++)
     {
-        strcpy(ConfigFile, ADCCONFIGFILES);
-    }
-    else 
-    {
-        return -1;
+        tmp[i + 1] = str[i];
     }
 
-    Config configSettings(ConfigFile);
+    tmp[i + 1] = '"';
+    tmp[i + 2] = '\0';
+
+    strcpy(str, tmp);
+}
+
+uint8_t setSensorInterval(char *str)
+{
+    int i = 0;
+    unsigned int j = 0;
+    struct sensorInfo *ptsensorInfo = NULL;
+
+    for (j = 0; j < sizeof(tsensorInfo)/sizeof(sensorInfo); j++)
+    {
+        ptsensorInfo = &tsensorInfo[j];
+
+        Config configSettings(ptsensorInfo->sensorPath);
+        
+        configSettings.Add("INTERVAL", str);
+
+        std::ofstream out(ptsensorInfo->sensorPath);
+
+        out << configSettings;
+    }
     
-    configSettings.Add("INTERVAL", str);
-
-    std::ofstream out(ConfigFile);
-    out << configSettings;
-
     return 0;
 }
 
-uint8_t  setSensorProperty(char (*str)[30], uint8_t sensortype)
+uint8_t  setSensorProperty(char (*str)[30])
 {
-    char ConfigFile[100] = {'\0'};
     char tmp[50] = {'\0'};
-    char threshType[10] = {'\0'};
+    char threshType[30] = {'\0'};
     int i = 0;
+    unsigned int j = 0;
+    struct sensorInfo *ptsensorInfo = NULL;
     std::string label;
-    
-    if (sensortype == ADC)
+
+    for (j = 0; j < sizeof(tsensorInfo)/sizeof(sensorInfo); j++)
     {
-        strcpy(ConfigFile, ADCCONFIGFILES);
-    }
-    else
-    {
-        return -1;
-    }
-   
-    Config configSettings(ConfigFile);
-    for(i = 0; i <= ADCMAXNUM; i++)
-    {
-        sprintf(tmp, "LABEL_in%d", i);
-        label = configSettings.Read(tmp, label);    
-        if (strcmp(label.c_str(), str[0]) == 0)
+        ptsensorInfo = &tsensorInfo[j];
+
+        Config configSettings(ptsensorInfo->sensorPath);
+
+        for(i = 0; i <= ptsensorInfo->sensorMaxNum; i++)
+        {
+            sprintf(tmp, "%s%d", ptsensorInfo->sensorNameLabel, i);
+            label = configSettings.Read(tmp, label);    
+            if (strcmp(label.c_str(), str[0]) == 0)
+                break;
+        }
+
+        if (i <= ptsensorInfo->sensorMaxNum)
             break;
     }
-    if (i >  ADCMAXNUM)
+
+    if (j == sizeof(tsensorInfo)/sizeof(sensorInfo))
     {
-        printf("The sensor_name: %s is not matched!\n", str[0]);
+        printf("cannot find the property sensortype\n");
         return -1;
     }
 
+    Config configSettings(ptsensorInfo->sensorPath);    
+
     if (strcmp(str[1], "WH") == 0)
     {
-        strcpy(threshType, "WARNHI_in");
+        strcpy(threshType, ptsensorInfo->sensorWHlabel);
     }
     else if (strcmp(str[1], "WL") == 0)
     {
-        strcpy(threshType, "WARNLO_in");
+        strcpy(threshType, ptsensorInfo->sensorWLlabel);
     }
     else if (strcmp(str[1], "CH") == 0)
     {
-        strcpy(threshType, "CRITHI_in");
+        strcpy(threshType, ptsensorInfo->sensorCHlabel);
     }
     else if (strcmp(str[1], "CL") == 0)
     {
-        strcpy(threshType, "CRITLO_in");
+        strcpy(threshType, ptsensorInfo->sensorCLlabel);
     }
     else
     {
-        printf("The threshType:%s is wrong tyep!", str[1]);
+        printf("The threshType:%s is wrong tyPE!", str[1]);
         return -1;
     }
 
     sprintf(tmp, "%s%d", threshType, i);
-
     configSettings.Add(tmp, str[2]);
     
-    std::ofstream out(ConfigFile);
+    std::ofstream out(ptsensorInfo->sensorPath);
+
     out << configSettings;
 
     return 0;
@@ -119,9 +150,10 @@ int main(int argc, char** argv)
 {
     int c;
     char str[3][30];
+
     while (1)
     {
-        c = getopt(argc, argv, "-ht:v:a:");
+        c = getopt(argc, argv, "-hv:q:t:");
         if (c < 0)
             break;
 
@@ -136,17 +168,30 @@ int main(int argc, char** argv)
                 strcpy(str[0], optarg);
                 strcpy(str[1], argv[optind]);
 
-                if (strncmp(str[0], "INTERVAL", 4) != 0)
-                {
-                    strcpy(str[2], argv[optind + 1]);
-                    setSensorProperty(str, ADC);
-                }
-                else 
-                {
-                    setSensorInterval(str[1], ADC);       
-                }
+                strcpy(str[2], argv[optind + 1]);
+                setSensorProperty(str);
 
+                break;
+
+            case 'q':
+                strcpy(str[0], optarg);
+                strcpy(str[1], argv[optind]);
+
+                strcpy(str[2], argv[optind + 1]);
+                addquota(str[0]);
+                addquota(str[2]);
+                setSensorProperty(str);
+
+                break;
+            case 't':
+                setSensorInterval(optarg);
+                break;
+
+            default:
+                printHelp();
                 break;
         }
     }
+
+    exit(0);
 }
